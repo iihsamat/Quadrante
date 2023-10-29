49b058a32b9d0f1fe7f6b01358daf84030423c61
Stefeson <stefesongomesdealmeidanetoo@gmail.com>
2023-10-29 10:49:24 -0300

    quadrante

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//Stefeson
+
+Console.Clear();
+
+double x, y;
+string local;
+
+Console.WriteLine("--- Quadrante Cartesiano ---");
+
+Console.WriteLine("Entre com o ponto desejado.");
+
+Console.Write("Cordenada x: ");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Cordenada y: ");
+y = Convert.ToDouble(Console.ReadLine());
+
+if (x == 0 && y ==0 ) local = "a origem";
+else if (x == 0) local = "o eixo das ordenadas";
+else if (y == 0) local = "o eixo das abcissas";
+else if (y > 0)
+{
+    if (x > 0)
+        local = "o quadrante 1";
+    else    
+        local = "o quadrante 2";
+    
+}
+else 
+{
+    if (x < 0)
+        local = "o quadrante 3";
+    else
+        local = "o quadrante 4";
+}
+
+Console.WriteLine($"O ponto ({x},{y}) fica n{local}");
